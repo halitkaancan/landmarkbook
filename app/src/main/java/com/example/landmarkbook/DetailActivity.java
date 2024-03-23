a3d5f39c5f125527d26e1de533e8bd5eb5c8e9f5
Kaan <halitkaancan@gmail.com>
2024-03-23 05:03:19 +0300

    first commit

---
+++ app/src/main/java/com/example/landmarkbook/DetailActivity.java
@@ -0,0 +1,29 @@
+package com.example.landmarkbook;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.landmarkbook.databinding.ActivityDetailBinding;
+
+public class DetailActivity extends AppCompatActivity {
+
+
+    private ActivityDetailBinding binding;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        //Landmark selectedLandmark = (Landmark) intent.getSerializableExtra("landmark");
+
+
+
+    }
+}
--- app/src/main/java/com/example/landmarkbook/MainActivity.java
+++ app/src/main/java/com/example/landmarkbook/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.landmarkbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.landmarkbook.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    ArrayList<Landmark> landmarkArrayList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        landmarkArrayList = new ArrayList<>();
+
+        Landmark pisa = new Landmark("Pisa","Italy",R.drawable.pisa);
+        Landmark eiffel = new Landmark("Eiffel","France",R.drawable.eiffel);
+        Landmark colloseum = new Landmark("Colloseum","Italy",R.drawable.colosseum);
+        Landmark londonBridge = new Landmark("London Bridge","UK",R.drawable.bridge);
+
+        landmarkArrayList.add(pisa);
+        landmarkArrayList.add(eiffel);
+        landmarkArrayList.add(colloseum);
+        landmarkArrayList.add(londonBridge);
+
+        //RecyclerView
+
+
     }
 }
